9fb8a8e84e1bcd9f70f92d6b267568a8ae64f736
yuseihaneishi <yuseihaneishi@icloud.com>
2024-12-07 14:12:43 +0900

    map

--- lesson.go
+++ lesson.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	n := make([]int, 3, 5)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+	fmt.Println(m)
+	fmt.Println(m["a"])
+	m["banana"] = 300
+	fmt.Println(m)
 
-	a := make([]int, 3)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+	m2 := make(map[string]int)
+	m2["pc"] = 5000
+	fmt.Println(m2)
 }
