d7e6f51d832193111c02c92b98dce863a464d535
yuseihaneishi <yuseihaneishi@icloud.com>
2024-12-08 00:26:21 +0900

    neru

--- lesson.go
+++ lesson.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
-	file, _ := os.Open("lesson.go")
-	defer file.Close()
-	date := make([]byte, 100)
-	file.Read(date)
-	fmt.Println(string(date))
+
 }
