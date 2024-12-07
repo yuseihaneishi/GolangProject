753ec0378a3245d2645a2584a1e3a36dd782fefb
yuseihaneishi <yuseihaneishi@icloud.com>
2024-12-07 15:34:44 +0900

    クロージャー

--- lesson.go
+++ lesson.go
@@ -2,19 +2,17 @@ package main
 
 import "fmt"
 
-func add(x, y int) (int, int) {
-	return x + y, x - y
-}
-
-func cal(price, item int) int {
-	result := price * item
-	return result
+func incrementGenerator() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
 }
 
 func main() {
-	r1, r2 := add(10, 20)
-	fmt.Println(r1, r2)
-
-	r3 := cal(100, 2)
-	fmt.Println(r3)
+	counter := incrementGenerator()
+	fmt.Println(counter())
+	fmt.Println(counter())
+	fmt.Println(counter())
 }
