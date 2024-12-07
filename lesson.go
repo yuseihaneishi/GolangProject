ce6e0676768ad0682704cee05bf4b9524691e36b
yuseihaneishi <yuseihaneishi@icloud.com>
2024-12-07 15:39:40 +0900

    pi * redius * redius func

--- lesson.go
+++ lesson.go
@@ -10,9 +10,20 @@ func incrementGenerator() func() int {
 	}
 }
 
+func circleArea(pi float64) func(radius float64) float64 {
+	return func(radius float64) float64 {
+		return radius * pi * radius
+	}
+}
+
 func main() {
 	counter := incrementGenerator()
 	fmt.Println(counter())
 	fmt.Println(counter())
 	fmt.Println(counter())
+
+	c1 := circleArea(3.14)
+	fmt.Println(c1(2))
+	c2 := circleArea(3)
+	fmt.Println(c2(2))
 }
