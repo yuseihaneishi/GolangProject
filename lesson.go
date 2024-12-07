b96042b268d29f823d1a37bf6a167b96449c9b94
yuseihaneishi <yuseihaneishi@icloud.com>
2024-12-07 12:30:45 +0900

    make,cap

--- lesson.go
+++ lesson.go
@@ -3,20 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	n := []int{1, 2, 3, 4, 5}
-	fmt.Println(n)
-	fmt.Println(n[2:4])
+	n := make([]int, 3, 5)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
 
-	n[2] = 100
-	fmt.Println(n)
-
-	var board = [][]int{
-		[]int{0, 1, 2},
-		[]int{3, 4, 5},
-		[]int{6, 7, 8},
-	}
-	fmt.Println(board)
-
-	n = append(n, 200, 300, 400, 500)
-	fmt.Println(n)
+	a := make([]int, 3)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
 }
