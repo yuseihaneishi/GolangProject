b381c7db3220db5dcd385d6f94250b89dd7027c1
yuseihaneishi <yuseihaneishi@icloud.com>
2024-12-07 15:53:20 +0900

    可変長変数

--- lesson.go
+++ lesson.go
@@ -2,28 +2,14 @@ package main
 
 import "fmt"
 
-func incrementGenerator() func() int {
-	x := 0
-	return func() int {
-		x++
-		return x
-	}
-}
-
-func circleArea(pi float64) func(radius float64) float64 {
-	return func(radius float64) float64 {
-		return radius * pi * radius
+func foo(params ...int) {
+	fmt.Println(len(params), params)
+	for _, param := range params {
+		fmt.Println(param)
 	}
 }
 
 func main() {
-	counter := incrementGenerator()
-	fmt.Println(counter())
-	fmt.Println(counter())
-	fmt.Println(counter())
-
-	c1 := circleArea(3.14)
-	fmt.Println(c1(2))
-	c2 := circleArea(3)
-	fmt.Println(c2(2))
+	foo(1, 2, 3, 4, 5)
+	foo(1, 2, 3)
 }
