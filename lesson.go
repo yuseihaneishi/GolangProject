6d8a17ae6504cd8c51b670d25e071d377eda082e
yuseihaneishi <yuseihaneishi@icloud.com>
2024-12-08 11:43:18 +0900

    error

--- lesson.go
+++ lesson.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	file, err := os.Open("./lesson.go")
+	if err != nil {
+		log.Fatalln("Error")
+	}
+	defer file.Close()
+	date := make([]byte, 100)
+	count, err := file.Read(date)
+	if err != nil {
+		log.Fatalln("Error")
+	}
+	fmt.Println(count, string(date))
+
+	if err = os.Chdir("test"); err != nil {
+		log.Fatalln("Error")
+	}
 }
