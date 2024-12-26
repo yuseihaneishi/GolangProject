43220ec58e76244bbfeefc6c98f9ca152de9b071
yuseihaneishi <yuseihaneishi@icloud.com>
2024-12-26 20:49:48 +0900

    kadai

---
+++ statement/kadai1.go
@@ -0,0 +1,19 @@
+package main
+
+// func main() {
+// 	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
+
+// 	var min int
+// 	for i, num := range l {
+// 		if i == 0 {
+// 			min = num
+// 			continue
+// 		}
+
+// 		if min >= num {
+// 			min = num
+// 		}
+// 	}
+
+// 	fmt.Println(min)
+// }
---
+++ statement/kadai2.go
@@ -0,0 +1,19 @@
+package main
+
+// func main() {
+// 	m := map[string]int{
+//         "apple":  200,
+//         "banana": 300,
+//         "grapes": 150,
+//         "orange": 80,
+//         "papaya": 500,
+//         "kiwi":   90,
+//     }
+
+// 	sum := 0
+// 	for _, v := range m {
+// 		sum += v
+// 	}
+
+// 	fmt.Println(sum)
+// }
