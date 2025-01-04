7a05631b0ef3f1e9348fecd1c4156d05cb5e4fc7
yuseihaneishi <yuseihaneishi@icloud.com>
2025-01-04 16:09:25 +0900

    test2

---
+++ struct/struct_test2.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct {
+	X, Y int
+}
+
+func (v Vertex) String() string {
+	return fmt.Sprintf("X is %v! Y is %v!", v.X, v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v)
+}
