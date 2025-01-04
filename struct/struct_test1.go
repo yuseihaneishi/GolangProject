a1e001ced97019a5471e519c5808148685ffdcb5
yuseihaneishi <yuseihaneishi@icloud.com>
2025-01-04 16:09:01 +0900

    test1

---
+++ struct/struct_test1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct {
+	X, Y int
+}
+
+func (v Vertex) Plus() int {
+	return v.X + v.Y
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Plus())
+}
