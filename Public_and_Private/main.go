6c4e59947390c1afb4237f6cc2faa9a7ba61f921
yuseihaneishi <yuseihaneishi@icloud.com>
2025-01-05 20:14:48 +0900

    public_private

--- Public_and_Private/main.go
+++ Public_and_Private/main.go
@@ -8,4 +8,11 @@ import (
 func main() {
 	s := []int{1, 2, 3, 4, 5}
 	fmt.Println(mylib.Average(s))
+
+	mylib.Say()
+	person := mylib.Person{"Mike", 22}
+	fmt.Println(person)
+
+	fmt.Println(mylib.Public)
+	//fmt.Println(mylib.pribate)
 }
