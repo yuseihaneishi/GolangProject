3f1b955b890ed09214ea35a6f26ca2e30fd8ecfa
yuseihaneishi <yuseihaneishi@icloud.com>
2025-01-05 17:24:01 +0900

    select_default_break

---
+++ goroutine/select_default_break.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+OuterLoop:
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			break OuterLoop
+		default:
+			fmt.Println(".")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+	fmt.Println("End.")
+}
